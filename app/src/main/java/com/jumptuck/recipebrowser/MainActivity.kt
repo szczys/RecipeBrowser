1b5b3fccbc642db9bcd94b9425f628d85281f1b3
Mike Szczys <szczys@hotmail.com>
2020-06-14 15:55:07 -0500

    Added about fragment and overflow menu

--- app/src/main/java/com/jumptuck/recipebrowser/MainActivity.kt
+++ app/src/main/java/com/jumptuck/recipebrowser/MainActivity.kt
@@ -2,10 +2,21 @@ package com.jumptuck.recipebrowser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.jumptuck.recipebrowser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
--- app/src/main/java/com/jumptuck/recipebrowser/RecipeListFragment.kt
+++ app/src/main/java/com/jumptuck/recipebrowser/RecipeListFragment.kt
@@ -1,12 +1,12 @@
 package com.jumptuck.recipebrowser
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.jumptuck.recipebrowser.databinding.FragmentRecipeListBinding
 
 /**
@@ -23,6 +23,17 @@ class RecipeListFragment : Fragment() {
         binding.button2.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_recipeList_to_singleRecipeFragment)
         )
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!, view!!.findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
