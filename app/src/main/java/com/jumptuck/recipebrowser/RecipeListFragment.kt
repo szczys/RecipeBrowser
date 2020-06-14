adbb49b0c2d92fe470f4c324958b872daaad9598
Mike Szczys <szczys@hotmail.com>
2020-06-14 11:47:22 -0500

    Added more fragments

--- app/src/main/java/com/jumptuck/recipebrowser/RecipeListFragment.kt
+++ app/src/main/java/com/jumptuck/recipebrowser/RecipeListFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import com.jumptuck.recipebrowser.databinding.FragmentRecipeListBinding
 
 /**
--- app/src/main/java/com/jumptuck/recipebrowser/SettingsFragment.kt
+++ app/src/main/java/com/jumptuck/recipebrowser/SettingsFragment.kt
@@ -6,19 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import com.jumptuck.recipebrowser.databinding.FragmentSingleRecipeBinding
+import com.jumptuck.recipebrowser.databinding.FragmentSettingsBinding
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SingleRecipeFragment.newInstance] factory method to
+ * Use the [SettingsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SingleRecipeFragment : Fragment() {
+class SettingsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var binding: FragmentSingleRecipeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_single_recipe, container, false)
-        return binding.root
+        var binding: FragmentSettingsBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 }
