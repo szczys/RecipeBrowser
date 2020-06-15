1dadc49fcc11a8c90f32f7b91aedcebfc8e6fbf9
Mike Szczys <szczys@hotmail.com>
2020-06-15 17:03:26 -0500

    Added nav animations; locked navigation drawer when not on RecipeListFragment

--- app/src/main/java/com/jumptuck/recipebrowser2/MainActivity.kt
+++ app/src/main/java/com/jumptuck/recipebrowser2/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.jumptuck.recipebrowser2.databinding.ActivityMainBinding
@@ -19,6 +21,16 @@ class MainActivity : AppCompatActivity() {
 
         val navController = this.findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, args: Bundle? ->
+            if (nd.id == nc.graph.startDestination) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            }
+            else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
+
         NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
