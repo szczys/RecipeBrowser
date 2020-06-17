44fc6a6e13bf3965b9e83766ae1aef5618763bce
Mike Szczys <szczys@hotmail.com>
2020-06-17 14:37:29 -0500

    Added protected variables

--- app/src/main/java/com/jumptuck/recipebrowser2/RecipeViewModel.kt
+++ app/src/main/java/com/jumptuck/recipebrowser2/RecipeViewModel.kt
@@ -1,20 +1,23 @@
 package com.jumptuck.recipebrowser2
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import timber.log.Timber
 
 class RecipeViewModel : ViewModel() {
 
-    val titleArray = MutableLiveData<ArrayList<String>>()
+    private val _titleArray = MutableLiveData<ArrayList<String>>()
+    val titleArray : LiveData<ArrayList<String>>
+        get() = _titleArray
 
     init {
         Timber.i("RecipeViewModel created")
-        titleArray.value = ArrayList<String>()
-        (titleArray.value)?.add("Whiskey Sour")
-        (titleArray.value)?.add("Bee's Knees")
-        (titleArray.value)?.add("Aperol Spritz")
+        _titleArray.value = ArrayList<String>()
+        (_titleArray.value)?.add("Whiskey Sour")
+        (_titleArray.value)?.add("Bee's Knees")
+        (_titleArray.value)?.add("Aperol Spritz")
     }
 
     override fun onCleared() {
@@ -23,7 +26,7 @@ class RecipeViewModel : ViewModel() {
     }
 
     fun addMenuItem() {
-        titleArray.value?.add("Success!!")
-        titleArray.value = titleArray.value //Force update for observers
+        _titleArray.value?.add("Success!!")
+        _titleArray.value = titleArray.value //Force update for observers
     }
 }
