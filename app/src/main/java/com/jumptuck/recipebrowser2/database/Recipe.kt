43f4ac81ceb4f835936c0cb0450bbd09bb9068b7
Mike Szczys <szczys@hotmail.com>
2020-06-18 00:37:46 -0500

    Added annotations for Room database library

--- app/src/main/java/com/jumptuck/recipebrowser2/database/Recipe.kt
+++ app/src/main/java/com/jumptuck/recipebrowser2/database/Recipe.kt
@@ -1,11 +1,23 @@
 package com.jumptuck.recipebrowser2.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "recipe_table")
 data class Recipe (
-    var recipeID: Long = 0L
+    @PrimaryKey(autoGenerate = true)
+    var recipeID: Long = 0L,
+    @ColumnInfo(name = "title")
     var title: String = "No Title Found",
+    @ColumnInfo(name = "body")
     var body: String = "No Body Found",
+    @ColumnInfo(name = "url")
     var link: String = "No URL Found",
+    @ColumnInfo(name = "dir")
     var directory: String = "No Directory Found",
+    @ColumnInfo(name = "updated")
     var date: String = "No Date Found",
+    @ColumnInfo(name = "fav")
     var favorite: Boolean = false
 )
