46dd85ced7d3fd6a44c1cb95acf723dba100f0e4
Mike Szczys <szczys@hotmail.com>
2020-06-18 20:26:04 -0500

    Implemented all DB calls in DAO

--- app/src/main/java/com/jumptuck/recipebrowser2/database/RecipeDatabaseDao.kt
+++ app/src/main/java/com/jumptuck/recipebrowser2/database/RecipeDatabaseDao.kt
@@ -1,6 +1,7 @@
 package com.jumptuck.recipebrowser2.database
 
 import android.database.Cursor
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
@@ -10,21 +11,29 @@ interface RecipeDatabaseDao {
     @Insert
     fun insert(recipe: Recipe)
 
-    @Query("UPDATE recipe_table SET fav=:fav WHERE recipeID=:key")
+    @Query("UPDATE recipe_table SET fav=:fav WHERE recipeID = :key")
     fun setFavorite(key: Long, fav: Boolean)
 
     @Query("SELECT count() FROM recipe_table WHERE fav=1")
     fun favoriteCount(): Int
 
-    fun isIn(title: String, date: String): Boolean
+    @Query("SELECT count() FROM recipe_table WHERE title = :title AND updated = :date")
+    fun isIn(title: String, date: String): Int
 
-    fun getRecipe(id: String): Recipe
+    @Query("SELECT * from recipe_table WHERE recipeID = :id")
+    fun getRecipe(id: Int): Recipe
 
-    fun getAll(): Cursor
+    @Query("SELECT * from recipe_table ORDER BY title ASC")
+    fun getAll(): LiveData<List<Recipe>>
+    //OLD CODE: fun getAll(): Cursor
 
-    fun getCategory(dir: String): Cursor
+    @Query("SELECT * from recipe_table WHERE dir = :cat ORDER BY title ASC")
+    fun getCategory(cat: String): LiveData<List<Recipe>>
+    //OLD CODE: fun getCategory(dir: String): Cursor
 
-    val favorites: Cursor
+    @Query("SELECT * from recipe_table WHERE fav = 1 ORDER BY title ASC")
+    //OLD CODE: val favorites: Cursor
 
+    @Query("DELETE FROM recipe_table")
     fun deleteAllRecipes()
 }
