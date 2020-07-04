4382eff5375cc486373cadeab09c8341f43fe036
Mike Szczys <szczys@hotmail.com>
2020-07-04 17:13:32 -0500

    Added listview

--- app/src/main/java/com/jumptuck/recipebrowser2/SettingsFragment.kt
+++ app/src/main/java/com/jumptuck/recipebrowser2/SettingsFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.databinding.DataBindingUtil
+import com.jumptuck.recipebrowser2.databinding.FragmentSettingsBinding
+import kotlinx.android.synthetic.main.fragment_settings.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -16,8 +20,20 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //var binding: FragmentSettingsBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        var binding: FragmentSettingsBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
+
+        val application = requireNotNull(this.activity).application
+        val view: View = inflater.inflate(R.layout.fragment_settings, container, false)
+
+        val listView = view.settings_list
+        var test_data = ArrayList<String>()
+        test_data.add("One")
+        test_data.add("Two")
+        test_data.add("Three")
+        val adapter = ArrayAdapter<String>(application, android.R.layout.simple_list_item_1, test_data)
+        listView.adapter = adapter
+
+
+        return view
     }
 }
