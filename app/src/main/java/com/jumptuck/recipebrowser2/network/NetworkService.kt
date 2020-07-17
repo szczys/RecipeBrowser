5712a62a53ef39a2255249b32bd25c9bed15f521
Mike Szczys <szczys@hotmail.com>
2020-07-17 12:35:23 -0500

    Added digest auth for apache file listings

--- app/src/main/java/com/jumptuck/recipebrowser2/network/NetworkService.kt
+++ app/src/main/java/com/jumptuck/recipebrowser2/network/NetworkService.kt
@@ -1,59 +1,37 @@
 package com.jumptuck.recipebrowser2.network
 
-import android.text.TextUtils
+import com.burgstaller.okhttp.AuthenticationCacheInterceptor
+import com.burgstaller.okhttp.CachingAuthenticatorDecorator
+import com.burgstaller.okhttp.digest.CachingAuthenticator
+import com.burgstaller.okhttp.digest.Credentials
+import com.burgstaller.okhttp.digest.DigestAuthenticator
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import kotlinx.coroutines.Deferred
-import okhttp3.Credentials
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Url
-
-/** Authentication approach uses code from:
- * https://futurestud.io/tutorials/android-basic-authentication-with-retrofit
- * **/
+import java.util.concurrent.ConcurrentHashMap
 
 private const val BASE_URL = "http://localhost/"
 
-private val httpClient = OkHttpClient.Builder()
+//Fixme: these credentials need to be passed in from Prefs
+val authenticator: DigestAuthenticator? = DigestAuthenticator(Credentials("someUsername", "somePassword"))
+
+val authCache: Map<String, CachingAuthenticator> = ConcurrentHashMap()
+val httpClient = OkHttpClient.Builder()
+    .authenticator(CachingAuthenticatorDecorator(authenticator, authCache))
+    .addInterceptor(AuthenticationCacheInterceptor(authCache))
+    .build()
 
 private var builder = Retrofit.Builder()
     .addConverterFactory(ScalarsConverterFactory.create())
     .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .client(httpClient)
     .baseUrl(BASE_URL)
 
 private var retrofit: Retrofit = builder.build()
-
-fun <S> createService(serviceClass: Class<S>?): S {
-    return createService(serviceClass, null, null)
-}
-
-fun <S> createService(
-    serviceClass: Class<S>?, username: String?, password: String?
-): S {
-    if (!TextUtils.isEmpty(username)
-        && !TextUtils.isEmpty(password)
-    ) {
-        val authToken: String = Credentials.basic(username, password)
-        return createService(serviceClass, authToken)
-    }
-    return createService(serviceClass, null)
-}
-
-fun <S> createService(
-    serviceClass: Class<S>?, authToken: String?
-): S {
-    if (!TextUtils.isEmpty(authToken)) {
-        val interceptor = AuthenticationInterceptor(authToken!!)
-        if (!httpClient.interceptors().contains(interceptor)) {
-            httpClient.addInterceptor(interceptor)
-            builder.client(httpClient.build())
-            retrofit = builder.build()
-        }
-    }
-    return retrofit.create(serviceClass)
-}
 
 interface NetworkService {
     @GET
