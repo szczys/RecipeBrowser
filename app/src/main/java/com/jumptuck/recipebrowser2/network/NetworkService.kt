0642f79fc98dfefc90ea032deeff0feebdc238b3
Mike Szczys <szczys@hotmail.com>
2020-07-17 11:19:44 -0500

    Added interceptor for https basic auth

--- app/src/main/java/com/jumptuck/recipebrowser2/network/NetworkService.kt
+++ app/src/main/java/com/jumptuck/recipebrowser2/network/NetworkService.kt
@@ -1,19 +1,59 @@
 package com.jumptuck.recipebrowser2.network
 
+import android.text.TextUtils
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import kotlinx.coroutines.Deferred
+import okhttp3.Credentials
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Url
 
+/** Authentication approach uses code from:
+ * https://futurestud.io/tutorials/android-basic-authentication-with-retrofit
+ * **/
+
 private const val BASE_URL = "http://localhost/"
 
-private val retrofit = Retrofit.Builder()
+private val httpClient = OkHttpClient.Builder()
+
+private var builder = Retrofit.Builder()
     .addConverterFactory(ScalarsConverterFactory.create())
     .addCallAdapterFactory(CoroutineCallAdapterFactory())
     .baseUrl(BASE_URL)
-    .build()
+
+private var retrofit: Retrofit = builder.build()
+
+fun <S> createService(serviceClass: Class<S>?): S {
+    return createService(serviceClass, null, null)
+}
+
+fun <S> createService(
+    serviceClass: Class<S>?, username: String?, password: String?
+): S {
+    if (!TextUtils.isEmpty(username)
+        && !TextUtils.isEmpty(password)
+    ) {
+        val authToken: String = Credentials.basic(username, password)
+        return createService(serviceClass, authToken)
+    }
+    return createService(serviceClass, null)
+}
+
+fun <S> createService(
+    serviceClass: Class<S>?, authToken: String?
+): S {
+    if (!TextUtils.isEmpty(authToken)) {
+        val interceptor = AuthenticationInterceptor(authToken!!)
+        if (!httpClient.interceptors().contains(interceptor)) {
+            httpClient.addInterceptor(interceptor)
+            builder.client(httpClient.build())
+            retrofit = builder.build()
+        }
+    }
+    return retrofit.create(serviceClass)
+}
 
 interface NetworkService {
     @GET
